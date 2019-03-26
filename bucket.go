0df1b1b7f5e5d3a4b8474ae066f22ba38d31486e
songweishuai <songweishuai@thunder.com.cn>
2019-03-26 17:30:03 +0800

    令牌桶模拟实现

---
+++ bucket.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var fillInterval = time.Second*3
+	var capacity = 100
+	var tokenBucket = make(chan struct{}, capacity)
+
+	fillToken := func() {
+		ticker := time.NewTicker(fillInterval)
+		for {
+			select {
+			case val := <-ticker.C:
+				fmt.Println("val:", val)
+				select {
+				case tokenBucket <- struct{}{}:
+				default:
+
+				}
+				//fmt.Println("current token cnt:", len(tokenBucket), time.Now())
+			}
+		}
+	}
+
+	go fillToken()
+	time.Sleep(time.Hour)
+}
