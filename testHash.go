9bd3a30c572f1ab3761038e8a21e5c42b00c2d73
songweishuai <songweishuai@thunder.com.cn>
2019-03-27 16:51:06 +0800

    测试hash散列

---
+++ testHash.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spaolacci/murmur3"
+)
+
+var bucketSize uint64 = 10
+
+func main() {
+	var bucketMap = map[uint64]int{}
+
+	for i := 15000000000; i < 15000000000+1000000; i++ {
+		//hashInt := murmur3.Sum64([]byte(fmt.Sprint(i))) % bucketSize
+		hashInt := murmur32(fmt.Sprint(i)) % bucketSize
+		bucketMap[hashInt]++
+	}
+	fmt.Println(bucketMap)
+}
+
+func murmur32(p string) uint64 {
+	return murmur3.Sum64([]byte(p))
+}
