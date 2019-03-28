37d653332dcf8791e4e0b9d9a5a288d983240ff7
songweishuai <songweishuai@thunder.com.cn>
2019-03-28 11:00:54 +0800

    随机算法比较，fisher-yates算法更好

--- testHash.go
+++ testHash.go
@@ -12,6 +12,7 @@ func main() {
 
 	for i := 15000000000; i < 15000000000+1000000; i++ {
 		//hashInt := murmur3.Sum64([]byte(fmt.Sprint(i))) % bucketSize
+		// 平均散列到10个桶中
 		hashInt := murmur32(fmt.Sprint(i)) % bucketSize
 		bucketMap[hashInt]++
 	}
--- testRand.go
+++ testRand.go
@@ -3,18 +3,59 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
-func main() {
-	// 初始化随机种子
-	rand.Seed(time.Now().UnixNano())
+//func main() {
+//	// 初始化随机种子
+//	rand.Seed(time.Now().UnixNano())
+//
+//	// 返回0-10的随机序列
+//	fmt.Println(rand.Perm(10))
+//	fmt.Println(rand.Perm(10))
+//	fmt.Println(rand.Perm(10))
+//	fmt.Println(rand.Perm(10))
+//	fmt.Println(rand.Perm(10))
+//	fmt.Println(rand.Perm(10))
+//
+//	rand.Int()
+//}
 
-	// 返回0-10的随机序列
-	fmt.Println(rand.Perm(10))
-	fmt.Println(rand.Perm(10))
-	fmt.Println(rand.Perm(10))
-	fmt.Println(rand.Perm(10))
-	fmt.Println(rand.Perm(10))
-	fmt.Println(rand.Perm(10))
+// 初始化随机种子
+func init() {
+	//rand.Seed(time.Now().UnixNano())
+}
+
+// 创建索引切片，随机索引切片,使用两两交换方式
+func shuffle1(slice []int) {
+	for i := 0; i < len(slice); i++ {
+		a := rand.Intn(len(slice))
+		b := rand.Intn(len(slice))
+		slice[a], slice[b] = slice[b], slice[a]
+	}
+}
+
+// 创建索引切片，随机索引切片，使用随机选取一个放到最后
+func shuffle2(slice []int) {
+	for i := len(slice) - 1; i > 0; i-- {
+		a := rand.Intn(i)
+		slice[i], slice[a] = slice[a], slice[i]
+	}
+}
+
+func main() {
+	var cnt1 = map[int]int{}
+	for i := 0; i < 1000000; i++ {
+		var s1 = []int{0, 1, 2, 3, 4, 5, 6}
+		shuffle1(s1)
+		cnt1[s1[0]]++
+	}
+
+	var cnt2 = map[int]int{}
+	for i := 0; i < 1000000; i++ {
+		var s2 = []int{0, 1, 2, 3, 4, 5, 6}
+		shuffle2(s2)
+		cnt2[s2[0]]++
+	}
+
+	fmt.Println(cnt1, "\n", cnt2)
 }
